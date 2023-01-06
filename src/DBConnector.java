c6039b3175bf0691b8454620e1b5a4af7e162bc7
Jakub Piekarski <jakubp2000@gmail.com>
2023-01-06 12:17:24 +0100

    podlaczenie bazy

---
+++ src/DBConnector.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBConnector {
+
+    static String url = "jdbc:mysql://localhost:3306/szpital";
+    static String username = "lekarz";
+    static String password = "hasloleka";
+
+
+    public static void connect()
+    {
+        try {
+
+            Connection connection = DriverManager.getConnection(url,username,password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from osoba");
+
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getString("imie"));
+            }
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        zaloguj();
+        DBConnector.connect();
+        //zaloguj();
     }
 
     public static void zaloguj()
