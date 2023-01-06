44f276a56a7032fa6414d3b2e42b4c7c1959046a
mati101811 <82444114+mati101811@users.noreply.github.com>
2023-01-06 11:39:55 +0100

    Add files via upload

---
+++ src/DBConnector.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBConnector {
+
+    static String url = "jdbc:mysql://localhost:3306/szpital";
+    static String username = "root";
+    static String password = "mysqlpass";
+
+
+    public static void connect()
+    {
+        try {
+            Connection connection = DriverManager.getConnection(url,username,password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from osoba");
+
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getString("imie"));
+            }
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
